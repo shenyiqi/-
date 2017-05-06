a6d7ebbfdd85bf8bda03387c20a5ed6be76eb2cf
shenyiqi <1264523595@qq.com>
2017-05-06 22:46:45 +0800

    5-6 更新

--- MyPets/Controllers/AdminController.cs
+++ MyPets/Controllers/AdminController.cs
@@ -20,46 +20,51 @@ namespace MyPets.Controllers
         [HttpGet]
         public ActionResult Baike()
         {
-           return View();
+            return View();
         }
         [HttpPost]
         [ValidateInput(false)]
-        public ActionResult Baike(string txttitle,string txtseries,string id_select,string txtdescribe, FormCollection fc)
+        public ActionResult Baike(string txttitle, string txtseries, string id_select, string txtdescribe, FormCollection fc)
         {
             MyPetsEntities db = new MyPetsEntities();
-            var content=fc["Bcontent"];
+            var content = fc["Bcontent"];
             HttpPostedFileBase postimg = Request.Files["txtimg"];
             string filepath = postimg.FileName;
-            if (filepath == "")
+            if (filepath == null)
             {
-                return Content("<script>; alert('用户名密码有误，或该账号不存在!'); history.go(-1) </ script >");
+                return Content("<script>; alert('请选择图片！'); history.go(-1) </ script >");
             }
-            string filename = filepath.Substring(filepath.LastIndexOf("//") + 1);
-            string serverpath = Server.MapPath("~/Content/Admin/Baike/img/upload/") + filename;
-            string relativepath = @"~/Content/Admin/Baike/img/upload/" + filename;
-            db.Baike.Add(new MyPets.Model.Baike()
+            else
             {
-                BaikeTitle = txttitle,
-                BaikeSeries = txtseries,
-                BaikeDescribe=txtdescribe,
-                BaikeType = id_select,
-                BaikeContent = content,
-                BaikeTime=DateTime.Now
-            });
-            db.SaveChanges();
-            return View();
+
+                string filename = filepath.Substring(filepath.LastIndexOf("//") + 1);
+                string serverpath = Server.MapPath("~/Content/Admin/Baike/img/upload/") + filename;
+                string relativepath = @"~/Content/Admin/Baike/img/upload/" + filename;
+                db.Baike.Add(new MyPets.Model.Baike()
+                {
+                    BaikeTitle = txttitle,
+                    BaikeSeries = txtseries,
+                    BaikeDescribe = txtdescribe,
+                    BaikeImg = relativepath,
+                    BaikeType = id_select,
+                    BaikeContent = content,
+                    BaikeTime = DateTime.Now
+                });
+                db.SaveChanges();
+                return View();
+            }
         }
         public ActionResult UserManagement()
         {
-            var userInfo = UserInfoServices.LoadEntities(u =>true).ToList();
-            ViewData.Model = userInfo;
-            return View();
+            var userInfo = UserInfoServices.LoadEntities(u => true).ToList();
+            return View(userInfo);
         }
-        public ActionResult Entry() 
+
+        public ActionResult Entry()
         {
             return View();
         }
-        public ActionResult GoodsEntry() 
+        public ActionResult GoodsEntry()
         {
             var goods = GoodsServices.LoadEntities(g => true).ToList();
             return View(goods);
--- MyPets/Controllers/BaikeController.cs
+++ MyPets/Controllers/BaikeController.cs
@@ -4,49 +4,84 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyPets.Model;
+using PagedList;
 namespace MyPets.Controllers
 {
     public class BaikeController : Controller
     {
         // GET: Baike
-        MyPets.IBLL.IBaikeServices BaikeServices=new MyPets.BLL.BaikeServices();
+        MyPets.IBLL.IBaikeServices BaikeServices = new MyPets.BLL.BaikeServices();
         MyPetsEntities db = new MyPetsEntities();
         public ActionResult Index()
         {
-            var dogBaike= BaikeServices.LoadEntities(b =>b.BaikeSeries=="狗系列").Take(6).ToList(); 
+            var dogBaike = BaikeServices.LoadEntities(b => b.BaikeSeries == "狗系列").Take(6).ToList();
             var dogTitle = db.Baike.OrderBy(b => b.BaikeSeries == "狗系列").Skip(6).Take(6).ToList();
 
-            var catBaike = BaikeServices.LoadEntities(b =>b.BaikeSeries=="猫系列").Take(6).ToList();
+            var catBaike = BaikeServices.LoadEntities(b => b.BaikeSeries == "猫系列").Take(6).ToList();
             var catTitle = db.Baike.OrderBy(b => b.BaikeSeries == "猫系列").Skip(6).Take(6).ToList();
 
             var littlepet = BaikeServices.LoadEntities(b => b.BaikeSeries == "小宠系列").Take(6).ToList();
-            var littleTitle= db.Baike.OrderBy(b => b.BaikeSeries == "小宠系列").Skip(6).Take(6).ToList();
+            var littleTitle = db.Baike.OrderBy(b => b.BaikeSeries == "小宠系列").Skip(6).Take(6).ToList();
 
             var waterpet = BaikeServices.LoadEntities(b => b.BaikeSeries == "水族系列").Take(6).ToList();
-            var waterTitle= db.Baike.OrderBy(b => b.BaikeSeries == "水族系列").Skip(6).Take(6).ToList();
-            ViewData["dog"] = dogBaike;   ViewData["dogtitle"] = dogTitle;
+            var waterTitle = db.Baike.OrderBy(b => b.BaikeSeries == "水族系列").Skip(6).Take(6).ToList();
+            ViewData["dog"] = dogBaike; ViewData["dogtitle"] = dogTitle;
 
-            ViewData["cat"] = catBaike;   ViewData["cattitle"] = catTitle;
+            ViewData["cat"] = catBaike; ViewData["cattitle"] = catTitle;
 
-            ViewData["littlepet"] = littlepet;  ViewData["littletitle"] = littleTitle;
+            ViewData["littlepet"] = littlepet; ViewData["littletitle"] = littleTitle;
 
-            ViewData["water"] = waterpet;    ViewData["watertitle"] = waterTitle;
+            ViewData["water"] = waterpet; ViewData["watertitle"] = waterTitle;
             return View();
         }
         public ActionResult Show(int id) //显示单个百科
-        { 
+        {
             var baike = BaikeServices.LoadEntities(b => b.BaikeId == id).FirstOrDefault();
             return View(baike);
         }
-       
-        public ActionResult Search(string txtBaikeTitle)//显示搜索的百科
+        public ActionResult GetType(string txttype, int? page) //传递百科类别分页参数
         {
-            //||b.BaikeType.Contains(txtBaikeTitle)||b.BaikeSeries.Contains(txtBaikeTitle)
-            ViewBag.search = txtBaikeTitle;
-            var baike = BaikeServices.LoadEntities(b => b.BaikeTitle.Contains(txtBaikeTitle) || b.BaikeType.Contains(txtBaikeTitle) || b.BaikeSeries.Contains(txtBaikeTitle)).ToList();
-            ViewBag.num = baike.Count();
-            return View(baike);
+            return RedirectToAction("ShowType", new { txttype, page });
         }
+        public ActionResult ShowType(string txttype, int? page) //显示类别百科
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var baike = BaikeServices.LoadEntities(b => b.BaikeType.Contains(txttype)).ToList();
+            ViewBag.num = baike.Count();
+            ViewData["type"] = txttype;
+            return View(baike.ToPagedList(pageNumber, pageSize));
 
+        }
+        public ActionResult GetSearch(string txtBaikeTitle, int? page)
+        {
+            return RedirectToAction("Search", new { txtBaikeTitle, page });
+        }
+        public ActionResult Search(string txtBaikeTitle, int? page)//显示搜索的百科
+        {
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            ViewBag.search = txtBaikeTitle;
+            var baike = BaikeServices.LoadEntities(b => b.BaikeTitle.Contains(txtBaikeTitle)).ToList();
+            ViewBag.num = baike.Count();
+            ViewData["search"] = txtBaikeTitle;
+            return View(baike.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult ShowAd1()
+        {
+            return View();
+        }
+        public ActionResult ShowAd2()
+        {
+            return View();
+        }
+        public ActionResult ShowAd3()
+        {
+            return View();
+        }
+        public ActionResult ShowAd4() 
+        {
+            return View();
+        }
     }
 }
