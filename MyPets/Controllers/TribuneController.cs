b398c9bae9e05a7edc9054b273276c18c9cec865
shayneW <shayneW@SHAYNE>
2017-06-11 18:43:50 +0800

    2017.6.11sy发帖，回复实现

--- MyPets/Controllers/TribuneController.cs
+++ MyPets/Controllers/TribuneController.cs
@@ -1,6 +1,7 @@
 ﻿using MyPets.DALFactory;
 using MyPets.IDAL;
 using MyPets.Model;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,73 +17,109 @@ namespace MyPets.Controllers
         IBLL.IUserInfoServices userInfoServices = new BLL.UserInfoServices();
         IDBSession db = new DBSession();
         // GET: Tribune
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             var goodPostings = postServices.LoadEntities(p => p.IsChoose == true).Take(10).ToList();
             ViewData["goodPostings"] = goodPostings;
             var topPostings = postServices.LoadEntities(p => p.IsTop == true).ToList();
             ViewData["topPostings"] = topPostings;
+
+            //分页控制最新发布的帖子
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
             var newPostings = postServices.LoadEntities(p => p.IsChoose == false && p.IsTop == false).OrderByDescending(p => p.PostTime).ToList();
-            ViewData["newPostings"] = newPostings;
-            return View();
+            //ViewData["newPostings"] = newPostings;
+            return View(newPostings.ToPagedList(pageNumber, pageSize));
+
         }
 
-        public ActionResult Postings(int id)
+        public ActionResult Postings(int id,int? page)
         {
-            return View();
+
+            var postFloor = responseServices.LoadEntities(r => r.PostId == id).ToList();
+            ViewBag.replyNum = postFloor.Count();
+            ViewBag.postId = id;
+            //楼主信息
+            var postTitle = postServices.LoadEntities(p => p.PostId == id).FirstOrDefault().PostTitle;
+            var postContent = postServices.LoadEntities(p => p.PostId == id).FirstOrDefault().PostContent;
+            var userId = postServices.LoadEntities(p => p.PostId == id).FirstOrDefault().UserId;
+            var userImg = userInfoServices.LoadEntities(u => u.UserId == userId).FirstOrDefault().UserImg;
+            var userName = userInfoServices.LoadEntities(u => u.UserId == userId).FirstOrDefault().UserName;
+            
+            ViewBag.postTitle = postTitle;
+            ViewBag.postContent = postContent;
+            ViewBag.userImg = userImg;
+            ViewBag.userName = userName;
+
+            //ViewData["postFloor"] = postFloor;
+            int pageSize = 10;//每十条一页
+            int pageNumber = (page ?? 1);
+            return View(postFloor.ToPagedList(pageNumber, pageSize));
         }
         [HttpPost]
         public ActionResult Postings(string postTitle, string postType, FormCollection fc)
         {
             var content = fc["AreaDescribe"];
-            string userName = Session["UserName"].ToString();
-            var userId = userInfoServices.LoadEntities(u => u.UserName == userName).FirstOrDefault().UserId;
-            var addquestion = postServices.AddEntity(new Post
+            if (Session["UserName"] != null)
             {
-                PostTitle = postTitle,
-                PostType = postType,
-                IsChoose = false,
-                IsTop=false,
-                PostContent = content,
-                PostTime = DateTime.Now,
-                UserId = userId
-            });
-            if (addquestion != null)
-            {
-                db.SaveChanges();
-                return View("Index");
+                string userName = Session["UserName"].ToString();
+                var userId = userInfoServices.LoadEntities(u => u.UserName == userName).FirstOrDefault().UserId;
+                var addquestion = postServices.AddEntity(new Post
+                {
+                    PostTitle = postTitle,
+                    PostType = postType,
+                    IsChoose = false,
+                    IsTop = false,
+                    PostContent = content,
+                    PostTime = DateTime.Now,
+                    UserId = userId
+                });
+                if (addquestion != null)
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else return Content("<script>alert('提交失败');history(-1)</script>");
             }
-            else return Content("<script>alert('提交失败');history(-1)</script>");
+            else return Content("<script>alert('您还未登录呢？！');history.go(-1);</script>");
         }
-        //public ActionResult Publish() //发帖
-        //{
-        //    if (Session["UserName"] != null)
-        //    {
-        //        return View();
-        //    }
-        //    else return Content("<script>if(confirm('要登录才能发帖,要去登录吗？')){ window.location.href = '/Register/Login'}else {history.go(-1);}</script>");
-        //}
-        //[HttpPost]
-        //[ValidateInput(false)]
-        //public ActionResult Publish(string InputTitle, string InputType, FormCollection fc) //发帖
-        //{
-        //    var content = fc["AreaDescribe"];
-        //     var addposting = postServices.AddEntity(new Post
-        //    {
-        //        PostTitle = InputTitle,
-        //        PostType = InputType,
-        //    //  IsChoose = false,
-        //        PostContent = content,
-        //        PostTime = DateTime.Now,
-        //        UserId = 1
-        //    });
-        //    if (addposting != null)
-        //    {
-        //        db.SaveChanges();
-        //        return View("Index");
-        //    }
-        //    else return Content("<script>alert('提交失败');history(-1)</script>");
 
-        //}
+
+        //回复帖子
+        [HttpPost]
+        public ActionResult ResponsePost(int id, FormCollection fc)
+        {
+            if (Session["UserName"] != null)//判定用户是否已登录
+            {
+                var postId = Convert.ToInt32(id);
+                var responseContent = fc["AreaDescribe"];
+                var receiverId = postServices.LoadEntities(p => p.PostId == id).FirstOrDefault().UserId;
+
+                //设置楼层数
+                var postFloor = responseServices.LoadEntities(r => r.PostId == id).ToList();
+                var floorNum = postFloor.Count() + 1;
+
+                if (responseContent != "")
+                {
+                    string userName = Session["UserName"].ToString();
+                    var senderId = userInfoServices.LoadEntities(u => u.UserName == userName).FirstOrDefault().UserId;
+                    var addRespon = responseServices.AddEntity(new Response
+                    {
+                        PostId = postId,
+                        ResponseContent = responseContent,
+                        ResponseTime = DateTime.Now,
+                        SenderId = senderId,
+                        ReceiverId = receiverId,
+                        ResponseFloor=floorNum
+                    });
+                    db.SaveChanges();
+                    return RedirectToAction("Postings", new { id=postId });
+                }
+                else return Content("<script>alert('内容不能为空！');history.go(-1);</script>");
+            }
+            else return Content("<script>alert('您还未登录呢？！');history.go(-1);</script>");
+        }
+
+        
     }
 }
